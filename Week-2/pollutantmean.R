37e9b79074342fe82a7df5dc50b5aa4d47accf9f
Cynthia Sun <cynthia0828@gmail.com>
2014-08-17 00:43:20 -0400

    Update pollutantmean.R

--- Week-2/pollutantmean.R
+++ Week-2/pollutantmean.R
@@ -35,4 +35,11 @@ pollutantmean <- function(directory, pollutant = "sulfate", id = 1:332) {
   result <- mean(mean_vector)
   return(round(result, 3)) 
 }
-
+##Test
+source("pollutantmean.R")
+pollutantmean("specdata", "sulfate", 1:10)
+## [1] 4.064
+pollutantmean("specdata", "nitrate", 70:72)
+## [1] 1.706
+pollutantmean("specdata", "nitrate", 23)
+## [1] 1.281
