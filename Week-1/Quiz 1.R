5fd01c26eec769f5fd8f92d85117bccc58a8bd9d
Cynthia Sun <cynthia0828@gmail.com>
2014-11-09 17:24:06 -0500

    Create Quiz 1.R

---
+++ Week-1/Quiz 1.R
@@ -0,0 +1,10 @@
+data<-read.table('H:/Desktop/Data Specialist/R programming/Quiz1/rprog_data_quiz1_data/hw1_data.csv',header=TRUE,sep=",")
+
+head(data)
+  Ozone Solar.R Wind Temp Month Day
+1    41     190  7.4   67     5   1
+2    36     118  8.0   72     5   2
+3    12     149 12.6   74     5   3
+4    18     313 11.5   62     5   4
+5    NA      NA 14.3   56     5   5
+6    28      NA 14.9   66     5   6
