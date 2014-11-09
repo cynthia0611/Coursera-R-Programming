69f9af89221ec97254d8a2c3978e30d267f70181
Cynthia Sun <cynthia0828@gmail.com>
2014-11-09 17:50:04 -0500

    Update Quiz 1.R

--- Week-1/Quiz 1.R
+++ Week-1/Quiz 1.R
@@ -9,3 +9,25 @@ head(data)
 4    18     313 11.5   62     5   4
 5    NA      NA 14.3   56     5   5
 6    28      NA 14.9   66     5   6
+
+#Question 12
+head(data,2)
+  Ozone Solar.R Wind Temp Month Day
+1    41     190  7.4   67     5   1
+2    36     118  8.0   72     5   2
+
+#Question 13
+nrow(data)
+[1] 153
+
+#Question 14
+tail(data,2)
+    Ozone Solar.R Wind Temp Month Day
+152    18     131  8.0   76     9  29
+153    20     223 11.5   68     9  30
+
+#Question 
+
+#Question 
+
+#Question 
