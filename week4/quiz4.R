a83dc3b06569049c808dac6f10bcf8d3f691ff8d
Cynthia Sun <cynthia0828@gmail.com>
2015-01-28 17:12:15 -0500

    Update quiz4.R

--- week4/quiz4.R
+++ week4/quiz4.R
@@ -1,9 +1,7 @@
-Q1:
-```
-What is produced at the end of this snippet of R code?
+Q1:What is produced at the end of this snippet of R code?
 set.seed(1)
 rpois(5, 2)
-```
+
 A vector with the numbers 1, 1, 2, 4, 1	
 Because the set.seed() function is used,rpois() will always output the same vector in this code.
 
