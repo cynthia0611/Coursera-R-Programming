e7ce6f0f8c70b4f4555438f629df4306d6dd9a3d
Cynthia Sun <cynthia0828@gmail.com>
2015-01-28 17:24:02 -0500

    Update quiz4.R

--- week4/quiz4.R
+++ week4/quiz4.R
@@ -16,11 +16,11 @@ What R function can be used to generate standard Normal random variables?
 
 Question 3
 When simulating data, why is using the set.seed() function important?
-It can be used to specify which random number generating algorithm R should use, ensuring consistency and reproducibility.
+# It can be used to specify which random number generating algorithm R should use, ensuring consistency and reproducibility.
 
 Question 4
 Which function can be used to evaluate the inverse cumulative distribution function for the Poisson distribution?
-qpois	
+# qpois	
 Probability distribution functions beginning with the `q`
 prefix are used to evaluate the quantile (inverse cumulative distribution) function.
 
@@ -30,14 +30,15 @@ set.seed(10)
 x <- rep(0:1, each = 5)
 e <- rnorm(10, 0, 20)
 y <- 0.5 + 2 * x + e
+# Generate data from a Normal linear model
 
 Question 6
 What R function can be used to generate Binomial random variables?
-rbinom
+# rbinom
 
 Question 7
 What aspect of the R runtime does the profiler keep track of when an R expression is evaluated?
-the function call stack
+# the function call stack
 
 Question 8
 Consider the following R code
@@ -48,14 +49,14 @@ Rprof(NULL)
 (Assume that y, x1, and x2 are present in the workspace.) Without running the code, 
 what percentage of the run time is spent in the 'lm' function, 
 based on the 'by.total' method of normalization shown in 'summaryRprof()'?
-
-100%
+# 100%
 
 Question 9
 When using 'system.time()', what is the user time?
+# It is the time spent by the CPU evaluating an expression
 
 Question 10
 If a computer has more than one available processor and R is able to take advantage of that, 
 then which of the following is true when using 'system.time()'?
-
+# elapsed time may be smaller than user time
 
